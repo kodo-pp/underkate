cd8c40c693f8b602c38d98fcb5eccf89306508b9
Alexander Korzun <korzun.sas@mail.ru>
2020-01-19 19:58:26 +0300

    Try to refactor load_room

--- underkate/overworld/load_room.py
+++ underkate/overworld/load_room.py
@@ -1,11 +1,11 @@
 from underkate.overworld.pass_map import PassMap
 from underkate.overworld.room import Room, Trigger, Event
 from underkate.script import load_script
-from underkate.texture import load_texture
+from underkate.texture import Texture, load_texture
 from underkate.vector import Vector
 
 from pathlib import Path
-from typing import Union, List, Dict, Callable, cast
+from typing import Union, List, Dict, Callable, Tuple, Any, Generator, cast
 
 import pygame as pg  # type: ignore
 import yaml
@@ -15,40 +15,85 @@ class RoomError(Exception):
     pass
 
 
-def load_room(path: Union[Path, str], prev_room_name: str) -> Room:
+class NoDefault:
+    pass
+
+
+def _get_item(
+    data: dict,
+    key: str,
+    Type: type,
+    default: Union[NoDefault, Any] = NoDefault()
+) -> Any:
+    if key not in data:
+        if isinstance(default, NoDefault):
+            raise RoomError(f'Missing required key: "{key}"')
+        return default
+    value = data[key]
+    if not isinstance(value, Type):
+        raise RoomError(f'Invalid type for "{key}": expected "{Type}", got "{type(value)}"')
+    return value
+
+
+def _load_room_data(path: Union[Path, str]) -> dict:
     if isinstance(path, str):
         path = Path(path)
 
     with open(path / 'room.yml') as f:
         data = yaml.safe_load(f)
 
-    room_name = cast(str, data['name'])
-    background_filename = path / cast(str, data['background'])
-    pass_map_filename = path / cast(str, data['pass_map'])
-    background = load_texture(background_filename)
-    pass_map = PassMap(pg.image.load(str(pass_map_filename)))
+    if not isinstance(data, dict):
+        raise RoomError('Invalid room file format: expected a dictionary at the top level')
+    return data
 
-    triggers: List[Trigger] = []
 
-    for trigger_description in data['triggers']:
-        x1, y1, width, height = trigger_description['rect']
-        if width < 0 or height < 0:
-            raise RoomError('Height or width of a trigger is negative')
+def _get_name(data: dict) -> str:
+    return _get_item(data, 'name', str)
 
-        event_handlers: Dict[Event, Callable] = {}
-        for event_name, script_filename in cast(
-            Dict[str, str],
-            trigger_description['events']
-        ).items():
-            script = load_script(script_filename, root=path)
-            event_handlers[Event(event_name)] = script
 
-        triggers.append(
-            Trigger(
-                pg.Rect(x1, y1, width, height),
-                event_handlers,
-            ),
-        )
+def _get_background(data: dict, root: Path) -> Texture:
+    filename = _get_item(data, 'background', str)
+    return load_texture(root / filename)
+
+
+def _get_pass_map(data: dict, root: Path) -> PassMap:
+    filename = _get_item(data, 'pass_map', str)
+    return PassMap(pg.image.load(str(root / filename)))
+
+
+def _get_triggers(data: dict, root: Path) -> List[Trigger]:
+    return [
+        _get_trigger(description, root)
+        for description in _get_item(data, 'triggers', list, [])
+    ]
+
+
+def _get_trigger(trigger_description: dict, root: Path):
+    # Left top corner coordinates and the dimensions (width and height)
+    # TODO: proper validation here
+    x, y, width, height = trigger_description['rect']
+    hitbox = pg.Rect(x, y, width, height)
+
+    def event_to_script_mapping_generator() -> Generator[Tuple[Event, Callable], None, None]:
+        for event_name, script_identifier in _get_item(trigger_description, 'events', dict).items():
+            if not isinstance(event_name, str):
+                raise RoomError('Event name is not a string')
+            if not isinstance(script_identifier, str):
+                raise RoomError('Script identifier is not a string')
+            yield Event(event_name), load_script(script_identifier, root=root)
+
+    event_handlers = dict(event_to_script_mapping_generator())
+    return Trigger(hitbox, event_handlers)
+
+
+def load_room(path: Union[Path, str], prev_room_name: str) -> Room:
+    if isinstance(path, str):
+        path = Path(path)
+    data = _load_room_data(path)
+    room_name = _get_name(data)
+    background = _get_background(data, path)
+    pass_map = _get_pass_map(data, path)
+    triggers = _get_triggers(data, path)
 
     data_scripts = cast(Dict[str, str], data.get('scripts', {}))
     scripts = {
