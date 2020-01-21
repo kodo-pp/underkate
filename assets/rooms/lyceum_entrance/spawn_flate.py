6040404760d675cb2e50a04da1d3972690aba547
Alexander Korzun <korzun.sas@mail.ru>
2020-01-22 00:27:41 +0300

    Widen Flate's hitbox

--- assets/rooms/lyceum_entrance/spawn_flate.py
+++ assets/rooms/lyceum_entrance/spawn_flate.py
@@ -8,6 +8,6 @@ from pygame import Rect
 
 async def main(*, root, script, **kwargs):
     texture = load_texture(root / 'flate' / 'overworld.png', scale=2)
-    flate = Object(Vector(400, 150), texture=texture, is_passable=False, hitbox=Rect(-60, -20, 120, 40))
+    flate = Object(Vector(400, 150), texture=texture, is_passable=False, hitbox=Rect(-60, -30, 120, 70))
     get_game().overworld.room.add_object(flate)
     flate.on_interact = lambda: get_game().overworld.room.run_script('flate_interact')
