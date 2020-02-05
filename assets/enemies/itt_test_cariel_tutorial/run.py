38bb11cce2233f437a0ad71395ac775487677580
Alexander Korzun <korzun.sas@mail.ru>
2020-02-05 19:36:40 +0300

    Add faces to ITT Test fight

--- assets/enemies/itt_test_cariel_tutorial/run.py
+++ assets/enemies/itt_test_cariel_tutorial/run.py
@@ -25,25 +25,27 @@ class Script(FightScript):
         cariel_neutral  = load_texture(self.battle.root / 'cariel' / 'face_neutral.png', scale=2)
         cariel_smiling  = load_texture(self.battle.root / 'cariel' / 'face_smiling.png', scale=2)
         cariel_thinking = load_texture(self.battle.root / 'cariel' / 'face_thinking.png', scale=2)
+        cariel_shouting = load_texture(self.battle.root / 'cariel' / 'face_shouting.png', scale=2)
+        cariel_begging  = load_texture(self.battle.root / 'cariel' / 'face_begging.png', scale=2)
         itt_test = TexturedSprite(Vector(400, 200), self.textures['itt_test'])
         get_game().current_game_mode.spawn(itt_test)
         font = load_font(Path('.') / 'assets' / 'fonts' / 'default')
         txt = DisplayedText([
             TextPage("IT Theory test attacks you", font),
             TextPage("... for example, right now", font, picture=cariel_neutral),
-            TextPage("But don't be afraid! Many of the tasks\nare simple, and...", font),
-            TextPage("it might be not too difficult... to solve...", font),
-            TextPage("...", font),
-            TextPage("Forgive me, please. I'm a bit worried\nabout that", font),
-            TextPage("Teachers commit themselves into designing\nthese problems and tasks", font),
-            TextPage("They try very hard to put the deadlines\ninto your schedule", font),
-            TextPage("And what is most important is that...\n", font),
-            TextPage("THE POWER OF THEIR SOULS LIES IN THESE\nPROBLEMS", font),
-            TextPage("...", font),
-            TextPage("So please...", font),
-            TextPage("Don't solve them that easily", font),
-            TextPage("I just don't want you to hurt anyone", font),
-            TextPage("Alright?", font),
+            TextPage("But don't be afraid! Many of the tasks\nare simple, and...", font, picture=cariel_smiling),
+            TextPage("it might be not too difficult... to solve...", font, picture=cariel_neutral),
+            TextPage("...", font, picture=cariel_thinking),
+            TextPage("Forgive me, please. I'm a bit worried\nabout that", font, picture=cariel_neutral),
+            TextPage("Teachers commit themselves into designing\nthese problems and tasks", font, picture=cariel_neutral),
+            TextPage("They try very hard to put the deadlines\ninto your schedule", font, picture=cariel_neutral),
+            TextPage("And what is most important is that...\n", font, picture=cariel_neutral),
+            TextPage("THE POWER OF THEIR SOULS LIES IN THESE\nPROBLEMS", font, picture=cariel_shouting),
+            TextPage("...", font, picture=cariel_thinking),
+            TextPage("So please...", font, picture=cariel_begging),
+            TextPage("Don't solve them that easily", font, picture=cariel_begging),
+            TextPage("I just don't want you to hurt anyone", font, picture=cariel_smiling),
+            TextPage("Alright?", font, picture=cariel_begging),
         ])
         await display_text(txt)
         menu = self.get_main_menu()
