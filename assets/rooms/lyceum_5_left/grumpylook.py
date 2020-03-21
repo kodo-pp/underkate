75dded6c9560b8a552ce5581bdb2007e15d4a16b
Alexander Korzun <korzun.sas@mail.ru>
2020-03-21 23:29:26 +0300

    Add lyceum_5_left

--- assets/rooms/lyceum_5_left/grumpylook.py
+++ assets/rooms/lyceum_5_left/grumpylook.py
@@ -1,18 +1,16 @@
 from underkate.global_game import get_game
 from underkate.load_text import load_text
 from underkate.scriptlib.common import display_text, sleep
-from underkate.scriptlib.gather import gather
 from underkate.texture import load_texture
 from underkate.animated_texture import AnimatedTexture
 from underkate.textured_walking_sprite import TexturedWalkingSprite
-from underkate.script import RoomScript
 
 
 async def main(root, **kwargs):
     overworld = get_game().overworld
     overworld.freeze()
 
-    await display_text(load_text('overworld/lyceum_2_left/grumpylook'))
+    await display_text(load_text('overworld/lyceum_5_left/grumpylook'))
 
     room = overworld.room
 
@@ -21,7 +19,6 @@ async def main(root, **kwargs):
     grumpylook_object.kill()
     del grumpylook_object
 
-    player = room.player
     texture = AnimatedTexture([load_texture(root / 'grumpylook' / 'overworld.png')], fps=1)
     grumpylook = TexturedWalkingSprite(
         pos = pos,
@@ -29,10 +26,10 @@ async def main(root, **kwargs):
         right = texture,
         front = texture,
         back = texture,
-        speed = player.speed,
+        speed = 280,
     )
     room.spawn(grumpylook)
 
-    await gather(grumpylook.walk_x(-240), player.walk_x(-240))
-    overworld.load_room('lyceum_5_left')
+    await grumpylook.walk_x(700)
+    grumpylook.kill()
     overworld.unfreeze()
