a6e190df7243586c69ecc2d3db95f6cebd70eaf7
Alexander Korzun <korzun.sas@mail.ru>
2020-03-23 23:32:37 +0300

    Add the third presentation stage

--- assets/rooms/lyceum_5_assembly_hall/presentation.py
+++ assets/rooms/lyceum_5_assembly_hall/presentation.py
@@ -6,3 +6,4 @@ from underkate.scriptlib.common import display_text
 async def main(**kwargs):
     await display_text(load_text('overworld/lyceum_5_assembly_hall/1'))
     await display_text(load_text('overworld/lyceum_5_assembly_hall/2'))
+    await display_text(load_text('overworld/lyceum_5_assembly_hall/3'))
