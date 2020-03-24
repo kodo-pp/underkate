45fb094928621caafe223903b3163687e341bcd7
Alexander Korzun <korzun.sas@mail.ru>
2020-03-25 01:58:26 +0300

    Add Algebroid and Geoma fights to the presentation

--- assets/rooms/lyceum_5_assembly_hall/presentation.py
+++ assets/rooms/lyceum_5_assembly_hall/presentation.py
@@ -9,4 +9,6 @@ async def main(**kwargs):
     await display_text(load_text('overworld/lyceum_5_assembly_hall/1'))
     await fight(load_enemy_battle_by_name('globby'))
     await display_text(load_text('overworld/lyceum_5_assembly_hall/2'))
+    await fight(load_enemy_battle_by_name('algebroid'))
+    await fight(load_enemy_battle_by_name('geoma'))
     await display_text(load_text('overworld/lyceum_5_assembly_hall/3'))
