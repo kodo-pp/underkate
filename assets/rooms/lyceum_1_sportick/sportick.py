37cc332fd6c1d9162495699ce0ad32fcd9b37700
Alexander Korzun <korzun.sas@mail.ru>
2020-03-29 22:36:11 +0300

    Add Sportick's pre-fight speech

--- assets/rooms/lyceum_1_sportick/sportick.py
+++ assets/rooms/lyceum_1_sportick/sportick.py
@@ -1,2 +1,6 @@
+from underkate.scriptlib.common import display_text
+from underkate.load_text import load_text
+
+
 async def main(**kwargs):
-    pass
+    await display_text(load_text('overworld/lyceum_1_sportick/sportick'))
